8895ec920138b7b7b31b517736b10d1858e70dfb
mc4785 <mc4785@nyu.edu>
2014-04-24 17:18:49 -0400

    sign up

---
+++ signup.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+    <form action="orders.php" method="post">
+        Username: <input type="text" name="uname"><br>
+        Password: <input type="text" name="password[[pass1]"><br>
+        Confirm Password: <input type="text" name="password[pass2]"><br>
+        Birthday: <input type="text" name="birth[year]">
+                  <input type="text" name="birth[month]">
+                  <input type="text" name="birth[day]"><br>
+        city: <input type="text" name="city"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
