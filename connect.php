6eff6f596a28c19dd759de33a240d4cdb2902180
Chrisgcy <crwzsh@gmail.com>
2014-05-03 19:46:30 -0400

    connect

--- connect.php
+++ connect.php
@@ -1,22 +1,32 @@
 <?php session_start();?>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <?php
-include("conn.php");
-$id=$_POST['id'];
-$pw=$_POST['pw'];
-
-$sql="select * from Users where uname='$id'";
-$result=mysql_query($sql);
-$row=@mysql_fetch_row($result);
-if($id!=null&&$pw!=null&&$row[1]==$id&&$row[2]==$pw)
-{
-$_SESSION['uname']=$id;
-echo 'login successfully!';
-echo '<meta http-equiv=REFRESH CONTENT=1;url=member.php>';
-}
-else
-{
-echo 'login unsuccessfully';
-echo '<meta http-equiv=REFRESH CONTENT=1;url=login.php>';
-}
-?>
+    include("conn.php");
+    $id=$_POST['id'];
+    $pw=md5['pw'];
+    
+    $id=stripslashes($id);
+    $pw=stripslashes($pw);
+    $id=mysql_real_escape_string($id);
+    $pw=mysql_real_escape_string($pw);
+    
+    
+    $sql="select * from Users where uname='$id'";
+    $result=mysql_query($sql);
+    $row=@mysql_fetch_row($result);
+    if($id!=null&&$pw!=null&&$row[1]==$id&&$row[2]==$pw)
+    {
+        $_SESSION['uname']=$id;
+        echo 'login successfully!';
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=member.php>';
+    }
+    else
+    {
+        echo 'login unsuccessfully';
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=login.php>';
+    }
+    echo $id;
+    echo $pw;
+    echo $row[1];
+    echo $row[2];
+    ?>
