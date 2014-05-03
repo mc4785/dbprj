b27313e6db32a361f77f16841566460553d0183f
mc4785 <mc4785@nyu.edu>
2014-05-02 23:46:15 -0400

    Add wall.php and header.inc.php

---
+++ header.inc.php
@@ -0,0 +1,25 @@
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Wildbook</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">My Wall</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="newsfeed.php">News Feed</a></li>
+            </ul>
+            <div class="pull-right">
+                <ul class="nav navbar-nav">
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
---
+++ wall.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">	
+    
+    <!-- Bootstrap core CSS -->
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css" rel="stylesheet">
+    <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+
+    <title>Wildbook</title>
+</head>
+
+<body>
+
+    <?php
+        include("header.inc.php");
+    ?>
+    <div class="container">
+  
+    </div>  
+
+
+</body>
+</html>
