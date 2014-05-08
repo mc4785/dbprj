2b884fd9acf97d0cf8380a7e25d7b412cb644b6e
Chrisgcy <crwzsh@gmail.com>
2014-05-08 16:30:11 -0400

    profile

---
+++ profile.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p.oblique {font-style:oblique;}
+ul
+{
+list-style-type:none;
+margin:-10px -10px;
+padding:0;
+overflow:hidden;
+}
+li
+{
+float:left;
+}
+a:link,a:visited
+{
+display:block;
+width:320px;
+font-weight:bold;
+color:#FFFFFF;
+background-color:#98bf21;
+text-align:center;
+padding:4px;
+text-decoration:none;
+text-transform:uppercase;
+}
+a:hover,a:active
+{
+background-color:#7A991A;
+}
+
+</style>
+</head>
+
+<body>
+<h2><p class="oblique">My Profile</p></h2>
+<ul>
+<li><a href="update.php">Home</a></li>
+<li><a href="friends.html">Friends</a></li>
+<li><a href="interests.php">Interests</a></li>
+<li><a href="#about">About</a></li>
+</ul>
+</body>
+</html>
