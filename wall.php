9ad72687f59c16c2d8dd8987d4aa0adbb7d8b524
mc4785 <mc4785@nyu.edu>
2014-05-08 07:12:03 -0400

    Modify wall.php
    +upload image & note
    +fetch my diary

--- wall.php
+++ wall.php
@@ -40,25 +40,23 @@
         <div class="row">
         <div class="column">
         <div class="col-md-4">
-
+    </div>
+    <div class="col-md-7">
+        <section>
+            <?php
+                include("post.inc.php");
+            ?>
+        </section>
+        <section>
+            <?php
+                include("fetchmypost.inc.php");
+            ?>
+        </section>
+    </div>
+<div class="col-md-5">
 </div>
-            <div class="col-md-7">
-                <section>
-                    <form action="postdiary.php" class="new_micropost" id="new_micropost" method="post">
-                            <textarea id="micropost_content" name="micropost[content]" placeholder="Give comment ..." style="width:100%">
-                            </textarea>
-                        <input class="btn btn-large btn-primary pull-right" name="commit" type="submit" value="POST" />
-                    </form>
-                </section>
-            </div>
-            <div class="col-md-5">
-                <?php
-                    // for each post
-                    //<ol class="microposts"></ol>
-                ?>
-            </div>
         </div>
-    </div>  
+    </div>
 
 </body>
 </html>
