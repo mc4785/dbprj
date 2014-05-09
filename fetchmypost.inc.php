c6101201b0c3a758dc2372ea5c170a602df658d0
mc4785 <mc4785@nyu.edu>
2014-05-09 05:08:38 -0400

    Modify fetchmypost.inc.php !! IMPORTANT see desc
    to make this work pls make sure you have the following procedure in
    your DB
    
    Delimiter //
    CREATE PROCEDURE listDiaryByUserWithLikes(IN username VARCHAR(40))
    BEGIN
    select D.*, L.cnt from
    (select Diary.*, nid, ncontent, iid, icontent,aid, acontent, vid,
    vcontent from Diary
    left join Note on Diary.did=Note.did
    left join Image on Diary.did=Image.did
    left join Audio on Diary.did=Audio.did
    left join Video on Diary.did=Video.did
    where Diary.uname=username) AS D
    left join
    (
    select did, count(*) as cnt from Like_diary group by did
    ) AS L
    on D.did=L.did;
    END
    //
    delimiter ;

--- fetchmypost.inc.php
+++ fetchmypost.inc.php
@@ -1,3 +1,29 @@
+<style>
+
+.diary-tool {
+    margin-bottom: 5px;
+    margin-top: 5px;
+}
+
+.diary-item:hover > div {
+    background-color: #eee;
+}
+
+.diary-item {
+    margin-bottom: 50px;
+}
+
+.diary-note, .comment-btn, .like-btn {
+    margin-left: 5px;
+}
+
+.like-counts {
+    position: absolute;
+    right: 0;    
+}
+
+</style>
+
 <div id="diaries">
 <?php
     
@@ -7,25 +33,67 @@ include("db_connect.inc.php");
     
 if($_SESSION['uname']!=null){
 
-    $fetch_diary_sql = "CALL listDiaryByUser('%s')";
+    $fetch_diary_sql = "CALL listDiaryByUserWithLikes('%s')";
     $sql = sprintf($fetch_diary_sql, $_SESSION['uname']);
-    
+    //error_log("fetch diary SQL: ".$sql);
+   
+    $s = "";  
     $result = mysql_query($sql);
     if ($result) {
-        $diary_a_format = "<a id='d%d' href='#top' class='property_item'><div class='row box list-group-item'>%s%s</div></a>";
-        $diary_img_format = "<div class='col-md-12 nopadding'><img class='col-lg-12 img-rounded' src='data:image/jpeg;base64, %s'/></div>";
-        $diary_note_format = "<div><dl><dd>%s</dd></dl></div>";
-        
-        while ($diary = mysql_fetch_array($result)) {
-            $diary_img = ($diary[9] === NULL) ? "" : sprintf($diary_img_format, $diary[11]);
-            $diary_note = sprintf($diary_note_format, $diary[8]);
-            $diary_a = sprintf($diary_a_format, $diary[0], $diary_img, $diary_note);
-            echo $diary_a;
+        $diary_item_format = "<div id='d%d' href='#top' class='diary-item'><div class='row box list-group-item'>%s%s</div>%s%s</div>";
+        $diary_img_format = "<div class='row nopadding'><img class='col-lg-12 img-rounded' src='data:image/jpeg;base64, %s'/></div>";
+        $diary_note_format = "<div class='row diary-note'><dl><dd>%s</dd></dl></div>";
+        $comment_btn = "<span><a class='comment-btn'>Comment</a></span>";
+        $like_cnt_format = "<span class='like-counts'><span><i class='glyphicon glyphicon-thumbs-up'></i></span><span id='ld%d'>%d</span></span>";
+        $diary_tool_format = "<div class='row diary-tool'><span><a class='like-btn' href=\"#\">Like</a></span>%s%s</div>";
+        $comment_holder_format = "<div id='dch%d' class='row'></div>";
+
+        while ($diary = mysql_fetch_assoc($result)) {
+            $diary_img = ($diary['iid'] === NULL) ? "" : sprintf($diary_img_format, $diary['icontent']);
+            $diary_note = sprintf($diary_note_format, $diary['ncontent']);
+
+            $like_cnt = sprintf($like_cnt_format, $diary['did'], $diary['cnt']);
+            $diary_tool = sprintf($diary_tool_format, $comment_btn, $like_cnt);
+            $comment_holder = sprintf($comment_holder_format, $diary['did']);
+            $diary_item = sprintf($diary_item_format, $diary['did'], $diary_img, $diary_note, $diary_tool, $comment_holder);
+            $s = $diary_item.$s;
         }
     }
+    echo $s;
     mysql_free_result($result);
 }
     
 mysql_close($connect);
 ?>
 </div>
+
+<script>
+$(".like-btn").click(function(){
+    var did = $(this).closest(".diary-item").attr('id').substring(1);
+    $.getJSON( "likediary.php", { "did": did }, function(data) {
+        if (data["result"] == "success") {
+            $("#ld"+data["did"]).html(data["cnt"])
+        }
+    });
+    return false;
+});
+
+$(".comment-btn").click(function(){
+    var did = $(this).closest(".diary-item").attr('id').substring(1);
+    $.getJSON( "getcomments.php", { "did": did }, function(data) {
+        if (data["result"] == "success") {
+            $("#dch"+data["did"]).append(data["cmt_form"]).append(data["comments"]);
+            $("#dch"+data["did"]).find("form").submit(function(){
+                $.post( "addcomment.php", $(this).serialize(), function(data){
+                    $("#dch"+data["did"]).find("ul").append(data["comment"])
+                    return false;
+                }, "json"); 
+                $(this)[0].reset();   
+                return false;
+            });
+        }
+        return false;
+    });
+    return false;
+});
+</script>
