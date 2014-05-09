16cec54cd45f1e6364cb2245776d941d67c54496
mc4785 <mc4785@nyu.edu>
2014-05-09 05:04:30 -0400

    modify fileupload.js

--- fileupload.js
+++ fileupload.js
@@ -1,13 +1,20 @@
 
 $(function () {
-	//'use strict';
+	'use strict';
 	
 	// Initialize the jQuery File Upload widget:
 	$('#fileupload').fileupload({
 		// Uncomment the following to send cross-domain cookies:
 		//xhrFields: {withCredentials: true},
-		url: ''//'fileuploadhandler.php'
-	});
+		url: 'fileuploadhandler.php',
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png|mov|mpg|vob|mp3)$/i,
+        maxNumberOfFiles: 1,
+	}).on('fileuploadalways', function (e, data) {
+        //alert("fff"+JSON.stringify(data));
+        location.reload();
+    }).on('fileuploadadd', function (e, data) {
+        $('#addfilebtn').addClass('disabled').find('span').text('Selected');
+    });
 	
 	// Enable iframe cross-domain access via redirect option:
 	$('#fileupload').fileupload(
@@ -32,13 +39,17 @@ $(function () {
 	        dataType: 'json',
 	        context: $('#fileupload')[0]
 	    }).always(function () {
+            //alert('alw');
             $(this).removeClass('fileupload-processing');
 	    }).done(function (result) {
+            //alert(JSON.stringify(result));
 		    $(this)
 				.fileupload('option', 'done')
 				.call(this, $.Event('done'), {result: result});
 		}).success(function () {
+            //alert('s');
 		}).error(function () {
+            //alert('err');
 		});
 	}
 });
