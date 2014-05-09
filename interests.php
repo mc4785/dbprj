86063a7f2e1b08dcf93f0dc07a6869005258d7a4
Chrisgcy <crwzsh@gmail.com>
2014-05-08 22:00:49 -0400

    interest2

--- interests.php
+++ interests.php
@@ -1,21 +1,16 @@
 <?php
-include("db_connect.inc.php");
-if(!$mysqli->query("drop procedure if exists userlikeActivity")||!mysqli->query("create procedure userlikeActivity(
-in username varchar(40),
-out id int(16))
-begin
-select actid into id from Like_activity where uname=username; end")){
-echo "Stored procedure creation failed: (".$mysqli->errno.")".$mysqli->error;
-}
-if(!mysqli->query("SET @id=' '")||!$mysqli->query("CALL userlikeActivity("Mary",@id)")){
-echo "CALL failed: (".$mysqli->errno.")".$mysqli->error;
-}
-
-if (!($res = $mysqli->query("SELECT @id as _p_out"))) {
-    echo "Fetch failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-
-$row = $res->fetch_assoc();
-echo $row['_p_out'];
-?>
-?>
+    include("db_connect.inc.php");
+    $sql="call userlikeActivity('$username')";
+    $result=mysql_query($sql);
+    echo "<table border="1">"
+    <tr>
+    <th>activity id</th>
+    </tr>
+    while($row=mysql_fetch_array($result)){
+    echo "<tr>";
+    echo "<td>".$row['id']."</td>";
+    echo "</tr>";
+    }
+    echo "</table>";
+    //mysql_query(call likeuserActivity('$username'));
+    ?>
