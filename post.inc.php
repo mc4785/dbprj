825a709cdcbf2c445e60d2d3f0e38e6fb3292213
mc4785 <mc4785@nyu.edu>
2014-05-10 17:21:32 -0400

    post.inc.php add selections for diary

--- post.inc.php
+++ post.inc.php
@@ -3,32 +3,148 @@
 	float:none;
 	margin: 0 auto;
 }
+
+textarea {
+    resize: none;
+}
+
+#locationField, #controls {
+position: relative;
+width: 100%;
+margin: -1 0 5 0;
+display:none;
+}
+#autocomplete {
+position: absolute;
+top: 0px;
+left: 0px;
+width: 100%;
+}
+.label {
+    text-align: right;
+    font-weight: bold;
+width: 100px;
+color: #303030;
+}
+#address {
+border: 1px solid #000090;
+background-color: #f0f0ff;
+width: 480px;
+padding-right: 2px;
+}
+#address td {
+font-size: 10pt;
+}
+.field {
+width: 99%;
+}
+.slimField {
+width: 80px;
+}
+.wideField {
+width: 200px;
+}
+#locationField {
+height: 20px;
+}
 </style>
+<link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
+
 
 <form id="fileupload" action="fileuploadhandler.php" method="POST" enctype="multipart/form-data">
 	<div class="row fileupload-buttonbar">
-		<div class="col-lg-7">
+		<div class="col-lg-12">
+
 			<!-- The fileinput-button span is used to style the file input field as button -->
-			<span id="addfilebtn" class="btn btn-success fileinput-button">
+			<span id="addfilebtn" class="btn btn-info fileinput-button">
 				<i class="glyphicon glyphicon-plus"></i>
 				<span>Add files...</span>
 				<input type="file" name="files[]" multiple>
 				<!-- The file input field used as target for the file upload widget -->
 				<!--<input id="fileupload" type="file" name="files[]" multiple>-->
 			</span>
+
+            <!-- Activity Button -->
+            <div class="dropdown btn-group">
+                <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+				    <span>&nbsp</span>
+				    <i class="glyphicon glyphicon-tags"></i>
+				    <span>&nbsp</span>
+				    <!--<span>Activity</span>
+                    <span class="caret"></span>-->
+                </button>
+                <ul class="dropdown-menu" role="menu">
+<?php
+    include("db_connect.inc.php");
+    $get_act_sql = "select * from activity";
+    $result = mysql_query($get_act_sql);
+    if ($result) {
+        $act_li_format = "<li class='act-item' id='actli%d'><a href='#'>%s</a></li>";
+        while ($act = mysql_fetch_assoc($result)) {
+            $act_li = sprintf($act_li_format, $act["actid"], $act["aname"]);
+            echo $act_li;
+            //error_log($act_li);
+        }
+    } else {
+        error_log(mysql_error());
+        error_log("get_act_sql err: ".$get_act_sql); 
+    }
+    mysql_close($connect);
+?>
+                </ul>
+            </div>
+
+            <!-- Location Button -->
+			<button type="button" class="btn btn-info add-loc-btn">
+				<span>&nbsp</span>
+				<i class="glyphicon glyphicon-map-marker"></i>
+				<span>&nbsp</span>
+			</button>
+
+            <!-- Privacy Button -->
+            <div class="dropdown btn-group">
+                <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+                    <span>&nbsp</span>
+                    <i class="glyphicon glyphicon-eye-open"></i>
+                    <span>&nbsp</span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li class='dv-item' id='dvfriend'><a href='#'>Friends</a></li>
+                    <li class='dv-item' id='dvfof'><a href='#'>FOFs</a></li>
+                    <li class='dv-item' id='dveveryone'><a href='#'>Everyone</a></li>
+                </ul>
+                <input type='hidden' id='dvinput' name='dv' value='friend'></input>
+            </div>
+
+            <!-- Post Button -->
 			<button type="submit" id="fileupload_submit" class="btn btn-primary start">
 				<i class="glyphicon glyphicon-upload"></i>
 				<span>Post&nbsp;&nbsp;</span>
 			</button>
 		</div>
 	</div>
-	<textarea id="micropost_content" name="micropost[content]" placeholder="Give comment ..." style="width:100%"></textarea>			
 
+    <!-- Note Input -->
+	<textarea id="micropost_content" name="micropost[content]" placeholder="Give comment ..." style="width:100%; height: 50px;"></textarea>
+
+    <!-- Location Input -->
+    <div id="locationField">
+        <input id="autocomplete" placeholder="Enter your address" onFocus="geolocate()" type="text"></input>
+        <input type='hidden' id='loc_id' name='loc_id' value=''></input>
+        <input type='hidden' id='loc_name' name='loc_name' value=''></input>
+        <input type='hidden' id='loc_lat' name='loc_lat' value=''></input>
+        <input type='hidden' id='loc_lon' name='loc_lon' value=''></input>
+    </div>
+
+    <!-- Show Activity Here -->
+    <div id="tag_holder" style="margin-top: 10px; margin-bottom:10px;"></div>
+    
 	<!-- The container for the uploaded files 
 	<div id="files" class="files"></div> -->
 	
 	<!-- The global progress bar -->
-	<div id="progress" class="progress">
+	<div id="progress" class="progress" style="display:none">
 		<div class="progress-bar progress-bar-success"></div>
 	</div>
 
@@ -132,7 +248,7 @@
 <!-- The Canvas to Blob plugin is included for image resizing functionality -->
 <script src="http://blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
 <!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<!--<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script> -->
 <!-- blueimp Gallery script -->
 <script src="http://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
 <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
@@ -153,4 +269,105 @@
 <script src="https://rawgit.com/blueimp/jQuery-File-Upload/master/js/jquery.fileupload-ui.js"></script>
 <!-- The main application script -->
 <script src="fileupload.js"></script>
+<script>
+$(".act-item").click(function() {
+    var actid = $(this).attr('id').substring(5),
+        actname = $(this).find("a").text();
+    var act_input_html = "<input type='hidden' name='activity[]' value=" + actid + ">",
+        tag_html = "<span class='label label-default' style='margin-right:5px'>" + actname + "</span>"
+    $("#fileupload").append(act_input_html);
+    $("#tag_holder").append(tag_html);
+});
+
+
+$(".add-loc-btn").click(function() {
+    $("#locationField").css("display", "block");
+});
+
+$(".dv-item").click(function() {
+    var visibility = $(this).attr('id').substring(2);
+    $("#dvinput").val(visibility);
+    //alert(visibility);
+});
+
+
+
+
+
+// This example displays an address form, using the autocomplete feature
+// of the Google Places API to help users fill in the information.
+
+var placeSearch, autocomplete;
+var componentForm = {
+street_number: 'short_name',
+route: 'long_name',
+locality: 'long_name',
+administrative_area_level_1: 'short_name',
+country: 'long_name',
+postal_code: 'short_name'
+};
+
+function initialize() {
+    // Create the autocomplete object, restricting the search
+    // to geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+                                                       /** @type {HTMLInputElement} */(document.getElementById('autocomplete')),
+                                                       { types: ['geocode'] });
+    // When the user selects an address from the dropdown,
+    // populate the address fields in the form.
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                                  fillInAddress();
+                                  });
+}
+
+// [START region_fillform]
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+    //alert(place.geometry.location.A);
+
+    //var id_html = "<input type='hidden' id=='loc_id' name='loc_id' value='" + place.id + "'></input>",
+    //    name_html = "<input type='hidden' id=='loc_name' name='loc_name' value='" + place.name + "'></input>",
+    //    lat_html = "<input type='hidden' id=='loc_lat' name='loc_lat' value='" + place.geometry.location[0] + "'></input>",
+    //    lon_html = "<input type='hidden' id=='loc_lon' name='loc_lon' value='" + place.geometry.location[1] + "'></input>";
+    $("#loc_id").val(place.id);
+    $("#loc_name").val(place.name);
+    $("#loc_lat").val(place.geometry.location.k);
+    $("#loc_lon").val(place.geometry.location.A);
+    
+    //for (var component in componentForm) {
+    //    document.getElementById(component).value = '';
+    //    document.getElementById(component).disabled = false;
+    //}
+    
+    // Get each component of the address from the place details
+    // and fill the corresponding field on the form.
+    //for (var i = 0; i < place.address_components.length; i++) {
+    //    var addressType = place.address_components[i].types[0];
+    //    if (componentForm[addressType]) {
+    //        var val = place.address_components[i][componentForm[addressType]];
+    //        document.getElementById(addressType).value = val;
+    //    }
+    //}
+}
+// [END region_fillform]
+
+// [START region_geolocation]
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+                                                 var geolocation = new google.maps.LatLng(
+                                                                                          position.coords.latitude, position.coords.longitude);
+                                                 autocomplete.setBounds(new google.maps.LatLngBounds(geolocation,
+                                                                                                     geolocation));
+                                                 });
+    }
+}
+// [END region_geolocation]
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+</script>
 
